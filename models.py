1a491e9d6259f7b15147c87726c28b31019b4047
abhishekverma555 <abhi5052005@gmail.com>
2025-06-02 14:01:19 +0530

    Update models.py

--- models.py
+++ models.py
@@ -1,5 +1,6 @@
-from sqlalchemy import Column, String
+from sqlalchemy import Column, String, ForeignKey, DateTime, func
 from sqlalchemy.dialects.postgresql import UUID
+from sqlalchemy.orm import relationship
 import uuid
 
 from database import Base
@@ -7,6 +8,21 @@ from database import Base
 class Note(Base):
     __tablename__ = "notes"
 
-    id = Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4)
-    title = Column(String, nullable=False)
-    content = Column(String, nullable=False)
+    id = Column(UUID(as_uuid=True), primary_key=True, index=True, default=uuid.uuid4)
+    title = Column(String, index=True)
+    content = Column(String)
+    user_id = Column(UUID(as_uuid=True), ForeignKey("users.id"))
+    created_at = Column(DateTime(timezone=True), server_default=func.now())
+    updated_at = Column(DateTime(timezone=True), onupdate=func.now())
+
+    owner = relationship("User", back_populates="notes")
+
+class User(Base):
+    __tablename__ = "users"
+
+    id = Column(UUID(as_uuid=True), primary_key=True, index=True, default=uuid.uuid4)
+    username = Column(String, unique=True, index=True)
+    hashed_password = Column(String)
+
+    notes = relationship("Note", back_populates="owner")
+
