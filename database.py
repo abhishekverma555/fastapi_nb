7dcdea16c37d4ddce97eb1068e9c004d1f4355e2
abhishekverma555 <abhi5052005@gmail.com>
2025-06-02 14:02:46 +0530

    Update database.py

--- database.py
+++ database.py
@@ -2,10 +2,16 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-# DB_ceredentials
 DATABASE_URL = "postgresql://postgres:BUBUP%40ru5@localhost/fastapi_notes"
 
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = declarative_base()
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
